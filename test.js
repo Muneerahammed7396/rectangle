7e1040d18fa6dfdd561ef2034aeadb7b86dda2c5
muneer ahammed <muneerahammed7396@gmail.com>
2022-08-27 21:51:01 +0530

    first commit

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-var parameater="mesarments of rectangle";
-var narthsouth=50;
-var eastwest=50;
-console.log(narthsouth+eastwest)
+var totalofmonths=("total months");
+var totalmonths=26;
+var twoyears=24;
+var remainig=2;
+console.log(totalmonths-twoyears)
